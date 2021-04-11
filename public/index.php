8f21dd0b5bb3b5b4da65eee0a28e2f3f30a14b2f
hallernsk <haller@ngs.ru>
2021-04-11 23:48:40 +0700

    new branch(modif_form)

--- public/index.php
+++ public/index.php
@@ -18,6 +18,31 @@ $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$fileName = 'dataUsers';
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['nickname' => '', 'email' => '', 'id' => '']
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
+$app->post('/users', function ($request, $response) use ($fileName) {
+    $user = $request->getParsedBodyParam('user');
+    $dataResult = json_decode(file_get_contents($fileName), true);
+    $dataResult[count($dataResult) + 1] = $user;
+    file_put_contents($fileName, json_encode($dataResult));
+
+    return $response->withRedirect('/users', 302);
+});
+
+$app->get('/users', function ($request, $response) use ($fileName) {
+    $usersAll = json_decode(file_get_contents($fileName), true);
+    $params = [
+        'users' => $usersAll
+    ];
+    return $this->get('renderer')->render($response, 'users/users.phtml', $params);
+});
 
 $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
@@ -25,16 +50,16 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
     // $this в Slim это контейнер зависимостей
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user');
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
-});
+})->setName('/');
 
-$app->get('/users', function ($request, $response) use ($users) {
+/*$app->get('/users', function ($request, $response) use ($users) {
     $term = $request->getQueryParam('term');
     $selectedUsers = $users;
     if (!is_null($term)) {
@@ -45,15 +70,17 @@ $app->get('/users', function ($request, $response) use ($users) {
     $params = ['users' => $selectedUsers, 'term' => $term];
 
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+}); */
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
+//$app->post('/users', function ($request, $response) {
+//    return $response->withStatus(302);
+//});
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
 });
+
+
 
 $app->run();
